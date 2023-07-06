4f93dfa5524ea9b25872e64a0df4ec51a9d3fb69
Angelillo15 <contact@angelillo15.es>
2023-07-06 15:09:44 +0200

    Added whitelists table

---
+++ ArticAntiVPN-API/src/main/java/es/angelillo15/artic/antivpn/api/models/WhiteListPlayers.java
@@ -0,0 +1,13 @@
+package es.angelillo15.artic.antivpn.api.models;
+
+import com.craftmend.storm.api.StormModel;
+import com.craftmend.storm.api.markers.Column;
+import com.craftmend.storm.api.markers.Table;
+import lombok.Data;
+
+@Data
+@Table(name = "articantivpn_whitelist_players")
+public class WhiteListPlayers extends StormModel {
+    @Column(name = "uuid")
+    private String uuid;
+}
