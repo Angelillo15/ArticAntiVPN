0094d1450accb84c7a31293c540765ed800a1207
Angelillo15 <contact@angelillo15.es>
2023-07-06 18:30:40 +0200

    Added databas loader and listener

--- ArticAntiVPN-Bungee/src/main/java/es/angelillo15/artic/antivpn/AntiVPN.java
+++ ArticAntiVPN-Bungee/src/main/java/es/angelillo15/artic/antivpn/AntiVPN.java
@@ -3,12 +3,18 @@ package es.angelillo15.artic.antivpn;
 import es.angelillo15.artic.antivpn.api.AntiVPNInstance;
 import es.angelillo15.artic.antivpn.api.Constants;
 import es.angelillo15.artic.antivpn.api.TextUtils;
+import es.angelillo15.artic.antivpn.api.cache.WhitelistPlayersCache;
+import es.angelillo15.artic.antivpn.api.config.Config;
 import es.angelillo15.artic.antivpn.api.config.ConfigLoader;
+import es.angelillo15.artic.antivpn.api.database.DataProvider;
+import es.angelillo15.artic.antivpn.api.database.PluginConnection;
+import es.angelillo15.artic.antivpn.api.models.WhiteListPlayers;
 import es.angelillo15.artic.antivpn.api.utils.ILogger;
+import es.angelillo15.artic.antivpn.listeners.AntiVPNListener;
 import es.angelillo15.artic.antivpn.utils.BungeeLogger;
 import es.angelillo15.artic.antivpn.utils.LibsLoader;
 import lombok.Getter;
-import lombok.Setter;
+import net.md_5.bungee.api.ProxyServer;
 import net.md_5.bungee.api.plugin.Plugin;
 
 import java.io.File;
@@ -17,9 +23,10 @@ import java.io.InputStream;
 public class AntiVPN extends Plugin implements AntiVPNInstance<Plugin> {
     @Getter
     private static AntiVPN instance;
-
     private static boolean debug = false;
     private static ILogger logger;
+    @Getter
+    private PluginConnection connection;
 
     @Override
     public boolean isDebug() {
@@ -48,12 +55,29 @@ public class AntiVPN extends Plugin implements AntiVPNInstance<Plugin> {
 
     @Override
     public void registerListeners() {
-
+        ProxyServer.getInstance().getPluginManager().registerListener(this, new AntiVPNListener());
+        WhitelistPlayersCache.loadWhitelistPlayers();
     }
 
     @Override
     public void loadDatabase() {
+        DataProvider dataProvider = DataProvider.valueOf(Config.Database.type().toUpperCase());
 
+        if (dataProvider == DataProvider.MYSQL) {
+            connection = new PluginConnection(
+                    Config.Database.host(),
+                    Config.Database.port(),
+                    Config.Database.database(),
+                    Config.Database.username(),
+                    Config.Database.password()
+            );
+
+            return;
+        }
+
+        connection = new PluginConnection(
+                getPluginDataFolder().getAbsolutePath()
+        );
     }
 
     @Override
