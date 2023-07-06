c88d1bede6882a04a01deb219148bc89097987dc
Angelillo15 <contact@angelillo15.es>
2023-07-06 15:13:23 +0200

    Added build constants

---
+++ ArticAntiVPN-API/src/main/java/es/angelillo15/artic/antivpn/api/Constants.java
@@ -0,0 +1,6 @@
+package es.angelillo15.artic.antivpn.api;
+
+public class Constants {
+    public static final String VERSION = "{version}";
+    public static final String COMMIT_TIME = "{date}";
+}
