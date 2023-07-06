91c19935e69c789ce2f4fefee18959ca08409924
Angelillo15 <contact@angelillo15.es>
2023-07-06 18:26:48 +0200

    Fix IPCache on local ips

--- ArticAntiVPN-API/src/main/java/es/angelillo15/artic/antivpn/api/cache/IPCache.java
+++ ArticAntiVPN-API/src/main/java/es/angelillo15/artic/antivpn/api/cache/IPCache.java
@@ -5,6 +5,7 @@ import com.github.benmanes.caffeine.cache.Cache;
 import com.github.benmanes.caffeine.cache.Caffeine;
 import com.google.gson.JsonElement;
 import com.google.gson.JsonParser;
+import es.angelillo15.artic.antivpn.api.AntiVPNInstance;
 import es.angelillo15.artic.antivpn.api.config.Config;
 import es.angelillo15.artic.antivpn.api.database.PluginConnection;
 import es.angelillo15.artic.antivpn.api.models.IPModel;
@@ -14,7 +15,7 @@ import kong.unirest.Unirest;
 import java.sql.SQLException;
 
 public class IPCache {
-    private static final String API_URL = "http://ip-api.com/json/{IP}?fields=country,countryCode,city,isp";
+    private static final String API_URL = "http://ip-api.com/json/{IP}?fields=country,countryCode,city,isp,proxy";
     private static final Cache<String, IPModel> cache = Caffeine.newBuilder()
             .maximumSize(Config.cacheMaxSize())
             .build();
@@ -30,15 +31,16 @@ public class IPCache {
             return cache.getIfPresent(ip);
         }
 
-        IPModel ipModel = IPModel.get(ip);
+        IPModel ipModel = getIpModelFromDB(ip);
 
         if (ipModel != null) {
             cache.put(ip, ipModel);
             return ipModel;
         }
 
-        return getIpModelFromAPI(ip);
+        return null;
     }
+
 
     public static IPModel getIpModelFromDB(String ip) {
         return IPModel.get(ip);
@@ -48,30 +50,45 @@ public class IPCache {
         HttpResponse<String> response = Unirest.get(API_URL.replace("{IP}", ip)).asString();
 
         if (response.getStatus() != 200) {
+            AntiVPNInstance.getLogger().error("Error getting IPModel from API: " + response.getStatus());
             return null;
         }
 
         JsonElement jsonElement = JsonParser.parseString(response.getBody());
 
-        if (jsonElement.isJsonObject()) {
+        if (!jsonElement.isJsonObject()) {
             return null;
         }
 
-        IPModel ipModel = new IPModel();
 
+
+        IPModel ipModel = new IPModel();
         ipModel.setIp(ip);
-        ipModel.setCountry(jsonElement.getAsJsonObject().get("country").getAsString());
-        ipModel.setCountryCode(jsonElement.getAsJsonObject().get("countryCode").getAsString());
-        ipModel.setCity(jsonElement.getAsJsonObject().get("city").getAsString());
-        ipModel.setIsp(jsonElement.getAsJsonObject().get("isp").getAsString());
+
+        try {
+            ipModel.setCountry(jsonElement.getAsJsonObject().get("country").getAsString());
+            ipModel.setCountryCode(jsonElement.getAsJsonObject().get("countryCode").getAsString());
+            ipModel.setCity(jsonElement.getAsJsonObject().get("city").getAsString());
+            ipModel.setIsp(jsonElement.getAsJsonObject().get("isp").getAsString());
+            ipModel.setProxy(jsonElement.getAsJsonObject().get("proxy").getAsBoolean());
+        } catch (Exception e) {
+            ipModel.setCountry("Localhost");
+            ipModel.setCountryCode("Localhost");
+            ipModel.setCity("Localhost");
+            ipModel.setIsp("Localhost");
+            ipModel.setProxy(false);
+        }
+
 
         Storm storm = PluginConnection.getStorm();
 
-        try {
-            storm.save(ipModel);
-        } catch (SQLException e) {
-            return null;
-        }
+        new Thread(() -> {
+            try {
+                storm.save(ipModel);
+            } catch (SQLException e) {
+                return;
+            }
+        }).start();
 
         cache.put(ip, ipModel);
 
