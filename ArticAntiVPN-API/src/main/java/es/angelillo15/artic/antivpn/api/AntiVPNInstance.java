1cc28188de64626c7fe7cb9b12bdc08eb95b1b10
Angelillo15 <contact@angelillo15.es>
2023-07-06 15:12:54 +0200

    Added antivpn instance interface

---
+++ ArticAntiVPN-API/src/main/java/es/angelillo15/artic/antivpn/api/AntiVPNInstance.java
@@ -0,0 +1,49 @@
+package es.angelillo15.artic.antivpn.api;
+
+import es.angelillo15.artic.antivpn.api.exceptions.PluginNotLoadedException;
+import es.angelillo15.artic.antivpn.api.utils.ILogger;
+import net.md_5.bungee.api.ProxyServer;
+import net.md_5.bungee.api.plugin.Plugin;
+import org.bukkit.Bukkit;
+import org.bukkit.plugin.java.JavaPlugin;
+
+import java.io.File;
+import java.io.InputStream;
+
+public interface AntiVPNInstance<P> {
+    static AntiVPNInstance<JavaPlugin> getInstance() {
+        AntiVPNInstance<JavaPlugin> instance = (AntiVPNInstance<JavaPlugin>) Bukkit.getPluginManager().getPlugin("ArticAntiVPN");
+
+        if (instance == null) {
+            throw new PluginNotLoadedException();
+        }
+
+        return instance;
+    }
+
+    static AntiVPNInstance<Plugin> getBungeeInstance() {
+        AntiVPNInstance<Plugin> instance = (AntiVPNInstance<Plugin>) ProxyServer.getInstance().getPluginManager().getPlugin("ArticAntiVPN");
+
+        if (instance == null) {
+            throw new PluginNotLoadedException();
+        }
+
+        return instance;
+    }
+
+    public boolean isDebug();
+    public void drawLogo();
+    public void loadConfig();
+    public void registerCommands();
+    public void registerListeners();
+    public void loadDatabase();
+    public void unregisterCommands();
+    public void unregisterListeners();
+    public void unloadDatabase();
+    public void reload();
+    public P getPluginInstance();
+    public ILogger getPLogger();
+    public File getPluginDataFolder();
+    public InputStream getPluginResourceAsStream(String path);
+    public void setDebug(boolean debug);
+}
